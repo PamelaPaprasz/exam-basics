949fd74d6753e11e6c2ace6317a1fb58d559db50
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-18 12:59:57 +0200

    final commit

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -3,16 +3,19 @@
 # Create basic unit tests for it with at least 3 different test cases
 
 def odd_average(numbers):
+    try:
+        odd_numbers = []
+        sum_odds = 0
     
-    odd_numbers = []
-    sum_odds = 0
+        for number in numbers:
+            if number %2 != 0:
+                odd_numbers.append(number)
     
-    for number in numbers:
-        if number %2 != 0:
-            odd_numbers.append(number)
-    
-    for number in odd_numbers:
-        sum_odds += number
-    return sum_odds / len(odd_numbers)
+        for number in odd_numbers:
+            sum_odds += number
+        return sum_odds / len(odd_numbers)
+        
+    except ZeroDivisionError:
+        return 0
                     
-print(odd_average([1, 2, 3, 4, 5]))
+print(odd_average([1, 2, 3, 4, 5, 8, 4, 3, 9]))
