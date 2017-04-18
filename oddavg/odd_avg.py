10667e1288b7dd0f5948a840a6ee6597aa86ee99
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-18 09:48:49 +0200

    feat(odd_avarage):it creates a list that contains only the odd numbers of the original list

--- oddavg/odd_avg.py
+++ oddavg/odd_avg.py
@@ -1,3 +1,17 @@
 # Create a function called `odd_average` that takes a list of numbers as parameter
 # and returns the average value of the odd numbers in the list
 # Create basic unit tests for it with at least 3 different test cases
+
+def odd_average(numbers):
+    
+    odd_numbers = []
+    
+    for number in numbers:
+        if number %2 != 0:
+            odd_numbers.append(number)
+    
+    length_of_odds = len(odd_numbers)
+            
+    
+    
+print(odd_average([1, 2, 3, 4, 5]))
