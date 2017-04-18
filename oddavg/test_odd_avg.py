884c48ee9dec6c39f339f3e4f77acff6c7f9888a
Pamela Paprasz <papraszpamela@gmail.com>
2017-04-18 10:29:57 +0200

    feat(test): tests are working for only odds and zero division but not if the result is a float number

---
+++ oddavg/test_odd_avg.py
@@ -0,0 +1,20 @@
+import unittest
+import odd_avg 
+
+class TestOddAvg(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_only_odds(self):
+        self.assertEqual(odd_avg.odd_average([5, 3, 5, 3]), 4)
+    
+    def test_one_even(self):
+        self.assertEqual(odd_avg.odd_average([6]), 0)
+    
+    def test_result_float(self):
+        self.assertEqual(odd_avg.odd_average([1, 4, 7, 2, 5, 3, 5, 3]), 4.16)
+    
+
+if __name__ == '__main__':
+    unittest.main()
+    
